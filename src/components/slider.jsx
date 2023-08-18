49cb3e5994a8d58c21371213c7caa292a29f0c5e
Chadi Rabii <chadirabii@gmail.com>
2023-08-18 21:11:33 +0100

    create the slider and fix the design..

---
+++ src/components/slider.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Settings } from "../password-generator";
+
+const Slider = ({ id, value, setValue }) => {
+  const handleSliderValueChange = (e) => {
+    const percent = (parseInt(e.target.value) / parseInt(e.target.max)) * 100;
+    e.target.style.setProperty("--percent", `${percent.toFixed(2)}%`);
+
+    setValue((prev) => ({ ...prev, length: parseInt(e.target.value) }));
+  };
+
+  return (
+    <div className="w-full">
+      <div className="flex justify-between mb-2 sm:mb-4">
+        <label htmlFor={id} className="text-base sm:text-body">
+          {id}
+        </label>
+        <span className="text-heading-M sm:text-heading-L text-neon-green">
+          {value}
+        </span>
+      </div>
+      <input
+        type="range"
+        id={id}
+        min={0}
+        max={20}
+        value={value}
+        className="w-full slider"
+        onChange={handleSliderValueChange}
+      />
+    </div>
+  );
+};
+
+export default Slider;
